742426d45d06326efa108d2f7d2885f3ad9a1045
Oscar Alonso <oscar.alonso@posibillian.tech>
2025-03-24 16:18:17 -0300

    Creación de docker-compose para el uso de mongo-db

--- src/plugins/envs.adapter.ts
+++ src/plugins/envs.adapter.ts
@@ -7,4 +7,9 @@ export const envs = {
   MAILER_SECRET_KEY: env.get('MAILER_SECRET_KEY').required().asString(),
   MAILER_SERVICE: env.get('MAILER_SERVICE').required().asString(),
   PROD: env.get('PROD').required().asBool(),
+
+  // CONEXION A MONGO
+  MONGO_DB_NAME: env.get('MONGO_DB_NAME').required().asString(),
+  MONGO_USERNAME: env.get('MONGO_USERNAME').required().asString(),
+  MONGO_PASSWORD: env.get('MONGO_PASSWORD').required().asString(),
 };
